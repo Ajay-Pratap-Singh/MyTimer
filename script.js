f09aaf92a2585ad9b6c669777ef01f35f4f5dbe3
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2022-02-17 01:42:06 +0530

    play pause cancel working with notif sound

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
-const button = document.getElementById("start-btn")
+const playPauseButton = document.getElementById("start-btn")
+const cancelButton = document.querySelector("#cancel-btn");
 const display = document.querySelector("#timer-spn");
-let duration = 3120;
+let durationReal=3;
+let duration = durationReal;
 let secondsRemaining;
 let min;
 let seconds;
 let startTime;
 let timer;
+
+function playSound(url) {
+    const audio = new Audio(url);
+    audio.play();
+}
+
 function startTimer() {
     let secondsElapsed = ( Date.now()/1000 | 0 ) - startTime;
-    let curr = Date.now()
     secondsRemaining = duration - secondsElapsed;
     min = secondsRemaining / 60 | 0;
     seconds = secondsRemaining % 60;
     display.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
     console.log(secondsElapsed)
-    if(duration==secondsElapsed)
+    if(duration==secondsElapsed){
         clearInterval(timer)
+        playSound("sounds/you-have-new-message-484.mp3")
+        playPauseButton.innerHTML = "Start";
+        timer = null;
+    }
 }
-button.addEventListener("click",()=>{
-    button.innerHTML=button.innerHTML=="Start"?"Pause":"Start";
+
+playPauseButton.addEventListener("click",()=>{
+    playPauseButton.innerHTML= playPauseButton.innerHTML=="Start" ?"Pause":"Start";
     startTime = Date.now()/1000 | 0;
     if(timer){
         clearInterval(timer);
         timer = null;
+        duration = secondsRemaining;
     }else{
         startTimer();
         timer = setInterval(startTimer,1000);
     }
+})
+
+cancelButton.addEventListener("click",()=>{
+    if(timer){
+        clearInterval(timer);
+        timer = null;
+    }
+    duration = durationReal
+    display.innerHTML = "00 : 00";
+    playPauseButton.innerHTML = "Start";
 })
