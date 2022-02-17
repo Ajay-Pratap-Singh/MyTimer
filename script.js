9c7707ea769340e2a3aececea25514a25edeaf33
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2022-02-17 22:53:15 +0530

    Version 0 done

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const playPauseButton = document.getElementById("start-btn")
-const cancelButton = document.querySelector("#cancel-btn");
-const display = document.querySelector("#timer-spn");
-let durationReal=3;
-let duration = durationReal;
+const playPauseButton = document.querySelector("#start-btn")
+const cancelButton = document.querySelector("#cancel-btn")
+const displayTimerDuration = document.querySelector("#timer-duration-spn")
+const displayTimerName = document.querySelector("#timer-name-spn")
+const nextTimerToggle = document.querySelector("#next-timer")
+let currentLevel = 0;
+let timerDurations=[
+    {name:"Work",duration:5},
+    {name:"Rest",duration:2}
+];
+let duration = timerDurations[currentLevel].duration;
 let secondsRemaining;
-let min;
-let seconds;
+let min = duration / 60 | 0;
+let seconds = duration % 60;
+displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
+displayTimerName.innerHTML = `(${timerDurations[currentLevel].name})`
 let startTime;
 let timer;
 
@@ -19,24 +27,35 @@ function startTimer() {
     secondsRemaining = duration - secondsElapsed;
     min = secondsRemaining / 60 | 0;
     seconds = secondsRemaining % 60;
-    display.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
-    console.log(secondsElapsed)
+    displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
     if(duration==secondsElapsed){
+        currentLevel=(currentLevel+1)%timerDurations.length;
         clearInterval(timer)
         playSound("sounds/you-have-new-message-484.mp3")
         playPauseButton.innerHTML = "Start";
         timer = null;
+        duration = timerDurations[currentLevel].duration;
+        min = duration / 60 | 0;
+        seconds = duration % 60;
+        displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
+        displayTimerName.innerHTML = `(${timerDurations[currentLevel].name})`
+        if(nextTimerToggle.checked && timerDurations.length == currentLevel+1){
+            playPauseButton.innerHTML= "Pause";
+            startTime = Date.now()/1000 | 0;
+            startTimer();
+            timer = setInterval(startTimer,1000);
+        }
     }
 }
 
 playPauseButton.addEventListener("click",()=>{
     playPauseButton.innerHTML= playPauseButton.innerHTML=="Start" ?"Pause":"Start";
-    startTime = Date.now()/1000 | 0;
     if(timer){
         clearInterval(timer);
         timer = null;
         duration = secondsRemaining;
     }else{
+        startTime = Date.now()/1000 | 0;
         startTimer();
         timer = setInterval(startTimer,1000);
     }
@@ -47,7 +66,10 @@ cancelButton.addEventListener("click",()=>{
         clearInterval(timer);
         timer = null;
     }
-    duration = durationReal
-    display.innerHTML = "00 : 00";
     playPauseButton.innerHTML = "Start";
+    duration = timerDurations[currentLevel].duration
+    min = duration / 60 | 0;
+    seconds = duration % 60;
+    displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
+    displayTimerName.innerHTML = `(${timerDurations[currentLevel].name})`
 })
