2a56cd5d9aace2f0812339639c035e4cc1e506d2
Ajay-Pratap-Singh <ajay.snp99@gmail.com>
2022-02-18 18:08:56 +0530

    Timer change functionality added

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ const cancelButton = document.querySelector("#cancel-btn")
 const displayTimerDuration = document.querySelector("#timer-duration-spn")
 const displayTimerName = document.querySelector("#timer-name-spn")
 const nextTimerToggle = document.querySelector("#next-timer")
+const timerContainer = document.querySelector("#timer-container")
+let selectTimerDuration;
+let dismissTimerChange;
+let confirmTimerChange;
+let timerChangeLabel;
 let currentLevel = 0;
 let timerDurations=[
-    {name:"Work",duration:3120},
-    {name:"Rest",duration:1020}
+    {name:"Work1",duration:3120},
+    {name:"Rest",duration:1020},
 ];
 let duration = timerDurations[currentLevel].duration;
 let secondsRemaining;
 let min = duration / 60 | 0;
 let seconds = duration % 60;
 displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
-displayTimerName.innerHTML = `(${timerDurations[currentLevel].name})`
+displayTimerName.innerHTML = `${timerDurations[currentLevel].name}`
 let startTime;
 let timer;
 
@@ -38,8 +43,8 @@ function startTimer() {
         min = duration / 60 | 0;
         seconds = duration % 60;
         displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
-        displayTimerName.innerHTML = `(${timerDurations[currentLevel].name})`
-        if(nextTimerToggle.checked && timerDurations.length == currentLevel+1){
+        displayTimerName.innerHTML = `${timerDurations[currentLevel].name}`
+        if(nextTimerToggle.checked && currentLevel != 0){
             playPauseButton.innerHTML= "Pause";
             startTime = Date.now()/1000 | 0;
             startTimer();
@@ -61,6 +66,54 @@ playPauseButton.addEventListener("click",()=>{
     }
 })
 
+displayTimerName.addEventListener("click",()=>{
+    dismissTimerChange = document.createElement("button")
+    dismissTimerChange.innerHTML = "Dismiss"
+    dismissTimerChange.addEventListener("click",()=>{
+        timerContainer.appendChild(displayTimerName);
+        confirmTimerChange.remove();
+        dismissTimerChange.remove();
+        timerChangeLabel.remove();
+        selectTimerDuration.remove();
+    })
+    confirmTimerChange = document.createElement("button")
+    confirmTimerChange.innerHTML = "Confirm"
+    confirmTimerChange.addEventListener("click",()=>{
+        currentLevel=selectTimerDuration.value;
+        clearInterval(timer)
+        playPauseButton.innerHTML = "Start";
+        timer = null;
+        duration = timerDurations[currentLevel].duration;
+        min = duration / 60 | 0;
+        seconds = duration % 60;
+        displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
+        displayTimerName.innerHTML = `${timerDurations[currentLevel].name}`
+        timerContainer.appendChild(displayTimerName);
+        confirmTimerChange.remove();
+        dismissTimerChange.remove();
+        timerChangeLabel.remove();
+        selectTimerDuration.remove();
+    })
+    timerChangeLabel = document.createElement("label",{for:"select-timer-duration"})
+    timerChangeLabel.innerHTML = "Select duration: "
+    selectTimerDuration = document.createElement("select",{name:"timer-duration",id:"select-timer-duration"})
+    for( i = 0 ; i < timerDurations.length ; i++ ){
+        if(currentLevel==i)
+        continue;
+        let opt = document.createElement("option")
+        opt.setAttribute("value",i);
+        let min = String(timerDurations[i].duration / 60 | 0).padStart(2,0);
+        let seconds = String(timerDurations[i].duration % 60).padStart(2,0);
+        opt.innerHTML = timerDurations[i].name + " - " + min + " : " + seconds
+        selectTimerDuration.appendChild(opt);
+    }
+    displayTimerName.parentElement.appendChild(timerChangeLabel)
+    displayTimerName.parentElement.appendChild(selectTimerDuration)
+    displayTimerName.parentElement.appendChild(confirmTimerChange)
+    displayTimerName.parentElement.appendChild(dismissTimerChange)
+    displayTimerName.remove();
+})
+
 cancelButton.addEventListener("click",()=>{
     if(timer){
         clearInterval(timer);
@@ -71,5 +124,5 @@ cancelButton.addEventListener("click",()=>{
     min = duration / 60 | 0;
     seconds = duration % 60;
     displayTimerDuration.innerHTML = String(min).padStart(2,0)+" : "+String(seconds).padStart(2,0);
-    displayTimerName.innerHTML = `(${timerDurations[currentLevel].name})`
+    displayTimerName.innerHTML = `${timerDurations[currentLevel].name}`
 })
